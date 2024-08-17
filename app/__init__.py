6d44c347b5bfc06672e4ef86b42668a72e83e992
= <=>
2024-08-17 15:51:49 +0530

    setting up logger on flask instance declaretion

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from app.routes import Router
 from flask_cors import CORS
 from mysql import connector
 from config import Config
+from .extentions.logger import Logger
 
+Logger.setup_logger()
 app = Flask(__name__)
 CORS(app, supports_credentials=True)  # Enable CORS for all routes
 
