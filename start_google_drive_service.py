ccf46ac74cbbfc11e1ea9124a6f79ffbdf89e113
= <=>
2024-08-17 11:23:30 +0530

    pushing changes

--- start_google_drive_service.py
+++ start_google_drive_service.py
@@ -1,11 +1,5 @@
 from app.services.bio_data_extraction import GoogleDrive
-from app.extentions.logger import Logger
-import time
 
 if __name__ == "__main__":
     _drive = GoogleDrive()
-    while True:
-        _drive.start_service()
-        sleepTime = 60 * 10
-        Logger.warning(f"Waiting for {sleepTime} seconds before checking again")
-        time.sleep(sleepTime)  # Wait for 10 minute before checking again
+    _drive.start_service()
